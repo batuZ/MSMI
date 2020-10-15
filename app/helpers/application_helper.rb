d4160adb830ae0d9d93de9a6c43d1af958439b40
batu <304701204@qq.com>
2020-10-15 12:10:12 +0800

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,7 @@ module ApplicationHelper
     begin
       msErr!('app_id或secret_key不合法', 1003) unless app?(params[:app_id])
       msErr!('app_id或secret_key不合法', 1003) unless app_info(params[:app_id])['secret_key'].eql?(params[:secret_key])
+      @current_user = {'app_id' => params[:app_id]}
     rescue StandardError
     end
   end
