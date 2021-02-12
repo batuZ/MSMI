31f34793f541cc05456636605f7079c120742515
batu <304701204@qq.com>
2021-02-12 12:35:50 +0800

    还是那个bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -124,7 +124,7 @@ module ApplicationHelper
         hold = {send_to: send_to, send_data: send_data}
         redis.setex("#{send_to}:messages:#{Time.now.to_i}", 1.month.to_i, hold.to_json)
         # apn(tag) if get_user(tag)['os_type'] == 1 && get_user(tag)['device_token'].present?
-        ApnJob.perform_later(current_user['app_id'], get_user(tag)['device_token'], redis.keys("#{u_key(tag)}:messages:*").count) if get_user(tag)['os_type'] == 1 && get_user(tag)['device_token'].present?
+        ApnJob.perform_now(current_user['app_id'], get_user(tag)['device_token'], redis.keys("#{u_key(tag)}:messages:*").count) if get_user(tag)['os_type'] == 1 && get_user(tag)['device_token'].present?
       end
       res += is_pushed
     end
@@ -144,63 +144,39 @@ module ApplicationHelper
   end
 
 #======================= apple apns =================================
-  
-  # 在主线程执可能造成崩溃，要放到jobs里
-  # def apn tag
-  #   pem = "config/keys/apn/#{current_user['app_id']}/product.pem"
-  #   apn_host = "https://api.push.apple.com:443"
 
-  #   unless  Rails.env.eql?('production')
-  #     pem = "config/keys/apn/#{current_user['app_id']}/sandbox.pem"
-  #     apn_host = "https://api.sandbox.push.apple.com:443"
-  #   end
-    
-  #   return(p 'no pem file') unless FileTest::exist?(pem)
-  #   # 把p12转成pem
-  #   # openssl pkcs12 -in aps_development.p12 -out ck_from_p12.pem -nodes
-  #   # 验证pem握手成功
-  #   # openssl s_client -connect api.development.push.apple.com:443 -cert "/Users/Batu/ck_from_p12.pem"
-  #   certificate = File.read(pem)
-  #   ctx         = OpenSSL::SSL::SSLContext.new
-  #   ctx.key     = OpenSSL::PKey::RSA.new(certificate)
-  #   ctx.cert    = OpenSSL::X509::Certificate.new(certificate)
+  def apn_client app_name
+    if $apn.nil?
+      if Rails.env.eql?('production')
+        # 生产环境
+        pem = "config/keys/apn/#{app_name}/product.pem"
+        apn_host = "https://api.push.apple.com:443"
+      else
+        # 开发环境
+        pem = "config/keys/apn/#{app_name}/sandbox.pem"
+        apn_host = "https://api.sandbox.push.apple.com:443"
+      end
 
-  #   # net/http2 gem
-  #   # https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns?language=objc
-  #   client = NetHttp2::Client.new(apn_host, ssl_context: ctx)
-  #   # client = NetHttp2::Client.new("https://api.push.apple.com : 443", ssl_context: ctx)
-    
-  #   # 发送内容
-  #   # https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/generating_a_remote_notification?language=objc
-  #   request = client.prepare_request(:post, "/3/device/#{get_user(tag)['device_token']}", 
-  #     body: {
-  #       aps: { 
-  #         badge: redis.keys("#{u_key(tag)}:messages:*").count, 
-  #         alert: '收到一条新消息',
-  #         sound: "bingbong.aiff"
-  #       },
-  #       # ms_data: ms_data # 不承载信息，只作静态提示
-  #     }.to_json,
-  #     headers: {
-  #       'apns-push-type' => 'alert',
-  #       'apns-expiration' => 0,
-  #       'apns-topic' => 'cn.mapplay.Mappy',
-  #       'apns-priority' => 10,
-  #     })
-  
-  #   # 返回错误处理
-  #   # https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/handling_notification_responses_from_apns?language=objc
-  #   request.on(:headers) { |headers| p headers }
-  #   request.on(:body_chunk) { |chunk|  p chunk }
-  #   request.on(:close) { puts "request completed!" }
+      return(p 'no pem file') unless FileTest::exist?(pem)
 
-  #   return 'test env will not do this' if Rails.env.eql?'test'
+   
+      # 把p12转成pem
+      # openssl pkcs12 -in aps_development.p12 -out ck_from_p12.pem -nodes
 
-  #   client.call_async(request)
-  #   client.join
-  #   client.close
-  # end
+      # 验证pem握手成功
+      # openssl s_client -connect api.development.push.apple.com:443 -cert "/Users/Batu/ck_from_p12.pem"
+      
+      certificate = File.read(pem)
+      ctx         = OpenSSL::SSL::SSLContext.new
+      ctx.key     = OpenSSL::PKey::RSA.new(certificate)
+      ctx.cert    = OpenSSL::X509::Certificate.new(certificate)
 
+      # net/http2 gem
+      # https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns?language=objc
+      $apn ||= NetHttp2::Client.new(apn_host, ssl_context: ctx)
+    end
+    return $apn
+  end
 
   #======================= groups =================================
 
--- app/jobs/apn_job.rb
+++ app/jobs/apn_job.rb
@@ -3,29 +3,6 @@ class ApnJob < ApplicationJob
   include ApplicationHelper
 
   def perform(app_name, device_token, badge)
-    pem = "config/keys/apn/#{app_name}/product.pem"
-    apn_host = "https://api.push.apple.com:443"
-
-    unless  Rails.env.eql?('production')
-      pem = "config/keys/apn/#{app_name}/sandbox.pem"
-      apn_host = "https://api.sandbox.push.apple.com:443"
-    end
-    
-    return(p 'no pem file') unless FileTest::exist?(pem)
-    # 把p12转成pem
-    # openssl pkcs12 -in aps_development.p12 -out ck_from_p12.pem -nodes
-    # 验证pem握手成功
-    # openssl s_client -connect api.development.push.apple.com:443 -cert "/Users/Batu/ck_from_p12.pem"
-    certificate = File.read(pem)
-    ctx         = OpenSSL::SSL::SSLContext.new
-    ctx.key     = OpenSSL::PKey::RSA.new(certificate)
-    ctx.cert    = OpenSSL::X509::Certificate.new(certificate)
-    
-    # net/http2 gem
-    # https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns?language=objc
-    @@client ||= NetHttp2::Client.new(apn_host, ssl_context: ctx)
-    # client = NetHttp2::Client.new("https://api.push.apple.com : 443", ssl_context: ctx)
-    
     # 发送内容
     # https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/generating_a_remote_notification?language=objc
     data_body = {
@@ -48,26 +25,20 @@ class ApnJob < ApplicationJob
 
     # 返回错误处理
     # https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/handling_notification_responses_from_apns?language=objc
-    
-
+    client = apn_client app_name
      # 异步请求
-    request = @@client.prepare_request(:post, "/3/device/#{device_token}",  body: data_body, headers: data_header)
-    # 会造成 Connection reset by peer (Errno::ECONNRESET)， 在不需要处理返回值时先注释掉
+    request = client.prepare_request(:post, "/3/device/#{device_token}",  body: data_body, headers: data_header)
     # request.on(:headers) { |headers| p headers }
     # request.on(:body_chunk) { |chunk|  p chunk }
     # request.on(:close) { puts "request completed!" }
-    @@client.call_async(request)
-    @@client.join
-   
-
+    client.call_async(request)
+    client.join
+    
     #同步请求
-    # @@client.call()
     # response = @@client.call(:post, "/3/device/#{device_token}",  body: data_body, headers: data_header)
     # response.ok?      # => true
     # response.status   # => '200'
     # response.headers  # => {":status"=>"200"}
     # response.body     # => "A body"
-   
-    # client.close
   end
 end
